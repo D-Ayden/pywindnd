aef95cb7d7606991f6afc8d76ec6a6c64f4dbc73
Ayden <2361018131@qq.com>
2022-09-05 21:55:22 +0800

    change main.py to windnd.py

--- windnd/windnd.py
+++ windnd/windnd.py
@@ -1,9 +1,31 @@
-def _func(ls) -> None:
+import typing
+
+
+def _func(ls: typing.List[str]) -> None:
     for s in ls:
         print(s)
 
 
-def hook_drop_files(tk, func=_func, force_unicode: bool = False):
+class OverError(Exception):
+    pass
+
+
+def drop_files(
+    tk, func: typing.Callable = _func, force_unicode: bool = False, init: int = 260
+) -> None:
+    """拖拽文件，获取文件绝对路径
+        drop file, get file's absolute path.
+
+    Args:
+        tk (_type_): tkinter 窗口或者对应的hwnd数字.
+        func (typing.Callable, optional): callback func, Defaults to _func.
+        force_unicode (bool, optional): Defaults to False.
+        init (int, optional): The init value of the buffer.
+
+    Raises:
+        Exception: _description_
+
+    """
     import ctypes
     import platform
     from ctypes.wintypes import DWORD
@@ -34,7 +56,7 @@ def hook_drop_files(tk, func=_func, force_unicode: bool = False):
         global files
         if msg == WM_DROP_FILES:
             count = func_DragQueryFile(typ(wp), -1, None, None)
-            file_buffer = create_buffer(260)
+            file_buffer = create_buffer(init)
             files = []
             for i in range(count):
                 func_DragQueryFile(typ(wp), i, file_buffer, ctypes.sizeof(file_buffer))
@@ -51,7 +73,7 @@ def hook_drop_files(tk, func=_func, force_unicode: bool = False):
     for i in range(limit_num):
         if i + 1 == limit_num:
             # *TODO 引发一个特定的错误，而不是一般的错误
-            raise Exception("Over hook limit number 200, for protect computer.")
+            raise OverError("Over hook limit number 200, for protect computer.")
         owp = f"old_wnd_proc_{i}"
         if owp not in globals():
             old, new = owp, f"new_wnd_proc_{i}"
@@ -63,3 +85,43 @@ def hook_drop_files(tk, func=_func, force_unicode: bool = False):
     ctypes.windll.shell32.DragAcceptFiles(hwnd, True)
     globals()[old] = GetWindowLong(hwnd, GWL_WND_PROC)
     SetWindowLong(hwnd, GWL_WND_PROC, globals()[new])
+
+
+if __name__ == "__main__":
+
+    def func(ls):
+        def _local_lnk(link):
+            """处理 link 指向的问题。"""
+            import platform
+
+            if platform.python_version().startswith("3") and type(link) is bytes:
+                try:
+                    _link = link.decode()
+                except Exception:
+                    _link = link.decode("gbk")
+            else:
+                _link = link
+            try:
+                import win32com.client
+
+                shell = win32com.client.Dispatch("WScript.Shell")
+                shortcut = shell.CreateShortCut(_link)
+                return shortcut.Targetpath if shortcut.Targetpath.strip() else _link
+            except Exception:
+                return _link
+
+        for i in ls:
+            print("deal link:", _local_lnk(i))
+
+    def test():
+        """
+        将 windows 桌面的图标拖拽到被挂钩的窗口内
+        对加载到的图标路径进行对应处理的函数。
+        """
+        import tkinter
+
+        tk = tkinter.Tk()
+        drop_files(tk, func)
+        tk.mainloop()
+
+    test()
